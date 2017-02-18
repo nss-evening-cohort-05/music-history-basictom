5870dfe81dcd0a6b2b78b7c37cae0a16af475659
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-02-18 13:55:25 -0600

    added array elements

--- js/music.js
+++ js/music.js
@@ -12,17 +12,16 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-var lastSong = songs.push("This is a new song at the end of an array.");
-var firstSong = songs.unshift("This is a new song at the front of an array.")
+songs.unshift("White and Nerdy > by Weird Al Yankovich on the album Straight Outta Lynwood");
+songs.push("Love Shack > by The B52's *@(! on the album Cosmic Thing");
 
-// var songs = songs[0,1,2,3,4].replace(">", "-");
+function findAndReplace(){
+  for(var i=0; i<songs.length; i++){
+    var newSongs = songs[i].replace(/[>]/g, "-").replace(/[!,*,@,(]/g, "");
+    var trackName = document.getElementById("track-container");
+    trackName.innerHTML += "<div>" + newSongs + "</div>";
+    // console.log(newSongs);
+  }
+}
 
-// for(var i=0; i<songs.length; i++){
-//   songs = songs[0].replace(">", "-");
-// }
-
-// function findAndReplace(replace){
-//
-// }
-
-console.log(songs);
+findAndReplace(songs);
